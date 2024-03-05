763620e18102fc4ce0eec734a3cb979db9e5cb13
Safian Tufail <220080680@aston.ac.uk>
2024-03-05 11:31:18 +0000

    I have completed lab 1-4

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,7 @@
+onePlusone = putStrLn("1 + 1 = 2")
+--putStrLn prints the string out
 main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    onePlusone
